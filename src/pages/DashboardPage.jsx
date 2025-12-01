85ff3f636a293502a4f31683a8bb4b21545a121b
giantfolarin <giantfolarin@gmail.com>
2025-12-01 22:41:08 +0100

    Remove website URL from share card footer

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -318,7 +318,6 @@ export default function DashboardPage() {
 
                   {/* Footer */}
                   <div className="pb-8 px-8 text-center">
-                    <p className="text-white text-2xl font-semibold mb-3">www.secretmessage.link</p>
                     <div className="flex items-center justify-center gap-2 text-yellow-300">
                       <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
